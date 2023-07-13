ad9104ab775eb20cc891ebad3b2820bf97b2bdd7
Mustafa Vural <vuralmustafa474@gmail.com>
2023-07-14 01:11:50 +0300

    C# progress bar ile proje yapımı
    C# Windows form üzerinden progress bar kullanarak pasta pişirme sürecinde nerelerde ne kadar vakit geçirildiği hesaplanmış oldu.

---
+++ pasta_pisirme_sürec_uygulamasi/pasta_pisirme_sürec_uygulamasi/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pasta_pisirme_sürec_uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value +=10;
+
+            if (progressBar1.Value % 100 == 0)
+            {
+                this.label1.BackColor = Color.Red;
+            }
+            if (progressBar1.Value == 100)
+            {
+
+                timer1.Stop();
+                timer2.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            progressBar2.Value += 5;
+            if (progressBar2.Value % 100 == 0)
+            {
+                this.label2.BackColor = Color.Blue;
+            }
+            if (progressBar2.Value == 100)
+            {
+                timer2.Stop();
+                timer3.Start();
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            progressBar3.Value += 20;
+            if(progressBar3.Value%100 == 0)
+            {
+                this.label3.BackColor = Color.Brown;
+            }
+            if (progressBar3.Value == 100)
+            {
+
+                timer3.Stop();
+                timer4.Start();
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            progressBar4.Value += 4;
+            if (progressBar4.Value % 100 == 0)
+            {
+                this.label4.BackColor = Color.Orange;
+            }
+            if(progressBar4.Value == 100)
+            {
+                timer4.Stop();
+                MessageBox.Show("pasta hazır hale geldi.");
+            }
+        }
+    }
+}
---
+++ pasta_pisirme_sürec_uygulamasi/pasta_pisirme_sürec_uygulamasi/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("pasta_pisirme_sürec_uygulamasi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pasta_pisirme_sürec_uygulamasi")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("d126501f-8f61-483d-8cb0-0fab9751c468")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
